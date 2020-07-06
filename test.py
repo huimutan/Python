4a9f00aac4be51edb6fcaf83bbb4d1dd1fb9d258
huimutan <xingdian119@hotmail.com>
2020-07-06 22:59:01 +0800

    小改动

--- test.py
+++
@@ -1,8 +0,0 @@
-def record(name, height, weight, age=5):
-  print('欢迎小朋友们！')
-  print('姓名：', name)
-  print('年龄：', age)
-  print('身高：', height, 'cm')
-  print('体重：', weight, 'kg')
-
-record("阿呆",115,25,4)
