6e3ba3957dabba007adb4f973e5f7ebf5d5f43cf
huimutan <xingdian119@hotmail.com>
2020-07-18 11:30:29 +0800

    测试文件

---
+++ test.py
@@ -0,0 +1,10 @@
+num = 23
+if num > 0:
+    print('num 大于 0')
+    if num % 2 == 0:  # % 用于取余数
+        print('num 是偶数')
+    else:
+        print('num 是奇数')
+else:
+    print('num 小于等于 0')
+print('num 小于等于 0')
